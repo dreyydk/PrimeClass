e049c56746b7c98c2bac258feff263235193220f
Andrei Bacin <andreib.pf@gmail.com>
2024-09-23 00:10:50 -0300

    Adjustments to Hero and creation of Navbar and Footer.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Footer, Navbar } from "@/components";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,6 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 
-export { Hero, CustomButton };
+export { Navbar, Footer, Hero, CustomButton };
